e5f66ea232f98bebde3fc8ecd0387d91f9698013
jacob0589 <alonsocb8995@gmail.com>
2023-02-25 01:43:32 +0000

    primer avance

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,31 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-
+import SecondsCounter from "./component/secondsCounter.jsx"
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let tiempoSegundos = 0
+let decenaSeconds=0
+let centenaSeconds =0 
+setInterval(() => {
+    tiempoSegundos = tiempoSegundos + 1
+    switch(true){
+        case tiempoSegundos>9:
+            tiempoSegundos=0
+            decenaSeconds++
+            break;
+        case decenaSeconds>5:
+            decenaSeconds=0
+            centenaSeconds++
+            break;
+        default:
+            break
+
+}
+
+
+
+
+
+ReactDOM.render(<SecondsCounter centenaSeconds={centenaSeconds} decenaSeconds={decenaSeconds} seconds={tiempoSegundos} />, document.querySelector("#app"));
+}, 100)
